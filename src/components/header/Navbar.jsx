a8b73b39dd5e7e47f3b858adb55734811f177de8
bassantahmed115 <ugs.161749@eng.suez.edu.eg>
2022-10-18 16:50:44 +0200

    navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+function clickme(){
+  alert('Welcome :)');
+}
 
 export const Navbar = () => {
   return (
-    <>
-    <div></div>
-    </>
+    <nav className="nav">
+      
+    <a to="/" className="site-name">
+      COFFEE SHOP
+    </a>
+    <ul>
+      <li><a to="/home"  className="home-style">Home</a></li>
+      <li><a to="/menu" className="site-title">Menu</a></li>
+      <li><a to="/about" className="site-title">About</a></li>
+      
+      
+    </ul>
+    <button onClick={clickme} className="site-button">Join Now </button>
+  </nav>
+    
   )
 }
